039611d3c6bc211ac5d53d5cb615794bc4a0bf77
Lucas Santos <lucasggs.com@gmail.com>
2023-04-27 19:12:48 -0300

    teste calculator2

---
+++ src/main/java/coordinator/app/Calculator2.java
@@ -0,0 +1,16 @@
+package coordinator.app;
+
+public class Calculator2 {
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    public int mux(int x, int y) {
+        return x * y;
+    }
+
+}
